00b0c967ee8b44524e3fedd9bbf74e00c225c318
peguerosdc <peguerosdc@gmail.com>
2021-06-15 01:38:30 -0500

    Retweeters done

--- auth_sample.py
+++ auth_sample.py
@@ -1,38 +1,54 @@
 from twitter_client import TwitterClient
 
+
 def get_api_keys():
     import os
+
     keys = {
-        "TWITTER_API_KEY" : os.getenv("TWITTER_API_KEY"),
-        "TWITTER_API_KEY_SECRET" : os.getenv("TWITTER_API_KEY_SECRET"),
-        "TWITTER_ACCESS_TOKEN" : os.getenv("TWITTER_ACCESS_TOKEN"),
-        "TWITTER_ACCESS_TOKEN_SECRET" : os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+        "TWITTER_API_KEY": os.getenv("TWITTER_API_KEY"),
+        "TWITTER_API_KEY_SECRET": os.getenv("TWITTER_API_KEY_SECRET"),
+        "TWITTER_ACCESS_TOKEN": os.getenv("TWITTER_ACCESS_TOKEN"),
+        "TWITTER_ACCESS_TOKEN_SECRET": os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
     }
     # Check that all the keys are set
-    if not (keys["TWITTER_API_KEY"] and keys["TWITTER_API_KEY_SECRET"]
-        and keys["TWITTER_ACCESS_TOKEN"] and keys["TWITTER_ACCESS_TOKEN_SECRET"]):
-        raise NameError("Twitter API keys not found as environment variables. See README for instructions.")
+    if not (
+        keys["TWITTER_API_KEY"]
+        and keys["TWITTER_API_KEY_SECRET"]
+        and keys["TWITTER_ACCESS_TOKEN"]
+        and keys["TWITTER_ACCESS_TOKEN_SECRET"]
+    ):
+        raise NameError(
+            "Twitter API keys not found as environment variables. See README for instructions."
+        )
     # If the keys are ok, proceed
     return keys
 
 
-if __name__ == '__main__':
-    # Get Twitter client with the current API keys
+def get_my_client():
     keys = get_api_keys()
-    client = TwitterClient(keys["TWITTER_API_KEY"], keys["TWITTER_API_KEY_SECRET"],
-            keys["TWITTER_ACCESS_TOKEN"], keys["TWITTER_ACCESS_TOKEN_SECRET"])
+    return TwitterClient(
+        keys["TWITTER_API_KEY"],
+        keys["TWITTER_API_KEY_SECRET"],
+        keys["TWITTER_ACCESS_TOKEN"],
+        keys["TWITTER_ACCESS_TOKEN_SECRET"],
+    )
+
+
+if __name__ == "__main__":
+    # Get Twitter client with the current API keys
+    client = get_my_client()
     # Print the users metadata as an example
     user = client.me()
     print(f"User: {user['name']} ({user['id']}). Followers: {user['followers_count']}")
     # Start creating a DB with the followers
     db = dict()
     for follower in client.get_followers():
-        follower_id = follower['id']
+        follower_id = follower["id"]
         db[follower_id] = follower
-        db[follower_id]['RTs'] = 0
-        db[follower_id]['replies'] = 0
-        db[follower_id]['quotes'] = 0
-        db[follower_id]['favs'] = 0
+        db[follower_id]["RTs"] = 0
+        db[follower_id]["replies"] = 0
+        db[follower_id]["quotes"] = 0
+        db[follower_id]["favs"] = 0
 
     # Get stats of every tweet in the timeline
     i = 0
@@ -40,10 +56,10 @@ if __name__ == '__main__':
         print(tweet)
 
         # Get retweeters of this tweet
-        for retweeter in client.get_retweeters(tweet['id']):
+        for retweeter in client.get_retweeters(tweet["id"]):
             # Update count in the db only for my followers
             if retweeter in db:
-                db[retweeter]['RTs'] += 1
+                db[retweeter]["RTs"] += 1
 
         # Get Favs
         # TODO
@@ -55,5 +71,7 @@ if __name__ == '__main__':
         # TODO
 
         i += 1
-        if i==19:
+        if i == 19:
             break
+
+    print(db)
--- twitter_client.py
+++ twitter_client.py
@@ -1,17 +1,38 @@
-# from TwitterAPI import TwitterAPI
-from TwitterAPI.TwitterAPI import TwitterAPI, TwitterPager
+from TwitterAPI import TwitterAPI, TwitterPager
+
 
 class TwitterClient(object):
-
     def __init__(self, api_key, api_key_secret, access_token, access_token_secret):
-        self.api = TwitterAPI(api_key, api_key_secret, access_token, access_token_secret, auth_type='oAuth1')
+        self.api1 = TwitterAPI(
+            api_key,
+            api_key_secret,
+            access_token,
+            access_token_secret,
+            api_version="1.1",
+            auth_type="oAuth1",
+        )
+        self.api2 = TwitterAPI(
+            api_key,
+            api_key_secret,
+            access_token,
+            access_token_secret,
+            api_version="2",
+            auth_type="oAuth1",
+        )
         # Init users data as it will be used in later requests
         self.me(refresh=True)
 
     def me(self, refresh=False):
         """ Request the data of the user matching this ACCESS_TOKEN and ACCESS_TOKEN_SECRET """
         if refresh:
-            req = self.api.request('account/verify_credentials', params={'include_entities':False, 'skip_status': True, 'include_email':False}, api_version='1.1')
+            req = self.api1.request(
+                "account/verify_credentials",
+                params={
+                    "include_entities": False,
+                    "skip_status": True,
+                    "include_email": False,
+                },
+            )
             if req.status_code != 200:
                 raise ValueError("Twitter credentials not valid. Unable to verify user")
             data = req.response.json()
@@ -20,38 +41,51 @@ class TwitterClient(object):
         return self.metadata
 
     def get_followers(self, page_size=1000):
-        """ Use v2 endpoint to get the followers of this account. Followers are paginated by the max amount which is 1000
+        """Use v2 endpoint to get the followers of this account. Followers are paginated by the max amount which is 1000
         See:
         https://developer.twitter.com/en/docs/twitter-api/users/follows/quick-start
         """
+
         def request_followers(next_token=None):
-            req = self.api.request(f"users/:{self.metadata['id']}/followers", params={'max_results':page_size, 'pagination_token':next_token}, api_version="2")
+            req = self.api2.request(
+                f"users/:{self.metadata['id']}/followers",
+                params={"max_results": page_size, "pagination_token": next_token},
+            )
             response = req.response.json()
-            return response.get('data',[]), response.get('meta', dict())
+            return response.get("data", []), response.get("meta", dict())
+
         # Start returning followers
         followers, meta = request_followers()
         while followers:
             for item in followers:
                 yield item
             # Request more followers
-            next_token = meta.get('next_token')
+            next_token = meta.get("next_token")
             if next_token:
                 followers, meta = request_followers(next_token)
             else:
                 followers = []
 
-    def get_tweets(self, page_size=1000):
+    def get_tweets(self, page_size=100):
         def request_tweets(next_token=None):
-            req = self.api.request(f"users/:{self.metadata['id']}/tweets", params={'max_results':page_size, 'pagination_token':next_token, 'tweet.fields':"in_reply_to_user_id"}, api_version="2")
+            req = self.api2.request(
+                f"users/:{self.metadata['id']}/tweets",
+                params={
+                    "max_results": page_size,
+                    "pagination_token": next_token,
+                    "tweet.fields": "in_reply_to_user_id",
+                },
+            )
             response = req.response.json()
-            return response.get('data',[]), response.get('meta', dict())
-        # Start returning followers
+            return response.get("data", []), response.get("meta", dict())
+
+        # Start returning tweets
         tweets, meta = request_tweets()
         while tweets:
             for item in tweets:
                 yield item
             # Request more tweets
-            next_token = meta.get('next_token')
+            next_token = meta.get("next_token")
             if next_token:
                 tweets, meta = request_tweets(next_token)
             else:
@@ -63,11 +97,33 @@ class TwitterClient(object):
 
     def get_retweeters(self, tweet_id):
         # Get the list of everyone who has retweeted this tweet
-        req = self.api.request(f"statuses/retweeters/ids", params={"id": tweet_id, "count":100, "stringify_ids":True}, api_version='1.1')
+        req = self.api1.request(
+            f"statuses/retweeters/ids",
+            params={"id": tweet_id, "count": 100, "stringify_ids": True},
+        )
         res = req.response.json()
         return res.get("ids", [])
 
     def get_quotes(self, tweet_id):
-        req = self.api.request(f"search/tweets", params={"q": tweet_id}, api_version='1.1')
+        req = self.api1.request(f"search/tweets", params={"q": tweet_id})
         # TODO
         return []
+
+    def get_replies(self, tweet_id):
+        # Consider this tweet is the beginning of a conversation
+        data = self.api2.request(
+            f"tweets/:{tweet_id}", params={"tweet.fields": "conversation_id"}
+        ).response.json()
+        # conversation_id = data['data'].get('conversation_id', None)
+        # if conversation_id:
+        # get thread
+        # thread_data = self.api.request(f"tweets/search/recent", params={'query': f"conversation_id:{tweet_id}"}, api_version="2").response.json()
+
+    def get_replies_to_me(self):
+        username = self.metadata["screen_name"]
+        # try to get next reply
+        pager = TwitterPager(
+            self.api, "search/tweets", {"q": f"to:{username} filter:replies"}
+        )
+        for reply in pager.get_iterator():
+            yield reply
