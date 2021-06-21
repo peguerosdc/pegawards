629b7699df76e3a64222a5e3fb4d8cb44c29eb16
peguerosdc <peguerosdc@gmail.com>
2021-06-21 01:12:42 -0500

    Fixing end time to be the last of the day

--- twitter_awards/utils.py
+++ twitter_awards/utils.py
@@ -2,16 +2,14 @@ from datetime import datetime
 import calendar
 
 
-def get_db_of_followers(followers):
+def get_db_of_followers(followers, operations):
     db = dict()
     for follower in followers:
         follower_id = follower["id"]
         db[follower_id] = follower
         db[follower_id]["id"] = follower_id
-        db[follower_id]["RTs"] = 0
-        db[follower_id]["replies"] = 0
-        db[follower_id]["quotes"] = 0
-        db[follower_id]["favs"] = 0
+        for label, _ in operations:
+            db[follower_id][label] = 0
     return db
 
 
@@ -23,6 +21,6 @@ def get_month_interval(year, timezone):
     for month in range(1, 13):
         _, last_day = calendar.monthrange(year, month)
         first_date = datetime(year, month, 1, tzinfo=timezone)
-        last_date = datetime(year, month, last_day, tzinfo=timezone)
+        last_date = datetime(year, month, last_day, 23, 59, 59, tzinfo=timezone)
         result += [(first_date.isoformat(), last_date.isoformat())]
     return result
